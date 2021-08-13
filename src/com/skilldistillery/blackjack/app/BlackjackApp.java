86fa7bcdea39937568fcb0a688fc101234c0ac19
jessicarmendariz <jessica.n.armendariz@gmail.com>
2021-08-13 17:41:25 -0600

    first try

---
+++ src/com/skilldistillery/blackjack/app/BlackjackApp.java
@@ -0,0 +1,19 @@
+package com.skilldistillery.blackjack.app;
+
+public class BlackjackApp {
+
+	public static void main(String[] args) {
+		BlackjackApp blackJack = new BlackjackApp();
+		blackjack.run();
+	}
+	
+	public void run() {
+		//I need a dealer - has a hand and a deck
+		//I need a Player - has a hand
+		//these are classes that need to be created
+		
+	// rules of the game - face up and face down, etc
+	
+	}
+
+}
